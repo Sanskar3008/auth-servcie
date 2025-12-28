c6513f941ea184c26f13abf3bac17cb7c6af1225
Sanskar30~ <sanskarghule230@gmail.com>
2025-12-28 18:33:11 +0530

    Initial commit

---
+++ src/main/java/com/rmo/auth_servcie/AuthServcieApplication.java
@@ -0,0 +1,13 @@
+package com.rmo.auth_servcie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServcieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServcieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rmo/auth_servcie/AuthServcieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmo.auth_servcie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServcieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
